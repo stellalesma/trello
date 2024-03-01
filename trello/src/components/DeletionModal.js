c7fab3624d77166087a2d2e0aa4feb6dad5b7ba5
stellalesma <akpedje.agbadjagan@epitech.eu>
2024-03-01 09:39:00 +0100

    [refactor] : use hover class instead of onMouseOver() and onMouseOut()

--- trello/src/components/DeletionModal.js
+++ trello/src/components/DeletionModal.js
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 
 function DeletionModal({modalName, text, onDelete, onClose}) {
-    const [backgroundColor, setBackgroundColor] = useState("bg-[#d60202]");
 
     return (
         <div className="w-[320px] p-[10px] mt-[5px] flex z-[10002] fixed rounded-[10px] box-border flex-col bg-white shadow-[0_4px_8px_rgba(0,0,0,0.1)]">
@@ -11,7 +9,7 @@ function DeletionModal({modalName, text, onDelete, onClose}) {
                 <IoMdClose className="text-[1.8em] p-[5px] cursor-pointer rounded-full hover:bg-[#E8E7E7]" onClick={onClose} />
             </div>
             <p className="leading-6 mb-[20px]">{text}</p>
-            <button className={`w-full text-white font-bold ${backgroundColor}`} onMouseOver={() => setBackgroundColor('bg-[#b80202]')} onMouseOut={() => setBackgroundColor("bg-[#d60202]")} onClick={onDelete}>Delete</button>
+            <button className="w-full text-white font-bold bg-[#d60202] hover:bg-[#b80202]" onClick={onDelete}>Delete</button>
         </div>
     );
 }
